004a5958d6c092a7f7c98173e757f27cc1ee12b7
diegolr-avanade <diego.lopez.reduello@avanade.com>
2024-04-04 11:30:16 +0200

    ✨ modulo 5

--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -4,15 +4,23 @@ public class Calculator
 {
     public int Calculate(int number1, int number2, string operation)
     {
-        if (operation == "/")
+        string notNullOperation = operation ?? throw new ArgumentNullException(nameof(operation));
+        if (notNullOperation == "/")
         {
-            return Divide(number1, number2);
+            try
+            {
+                return Divide(number1, number2);
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("logging...");
+                throw new ArithmeticException("An error occurred during calculaiton.",ex);
+            }
         }
         else
         {
             throw new ArgumentOutOfRangeException(nameof(operation), "The mathematical operator is not supported.");
-            // Console.WriteLine("Unknown operation.");
-            // return 0;
         }
     }
 
--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -3,6 +3,9 @@ using static System.Console;
 
 // Note: Additional input validation omitted for brevity
 
+AppDomain currentAppDomain = AppDomain.CurrentDomain;
+currentAppDomain.UnhandledException += new UnhandledExceptionEventHandler(HandleException);
+
 WriteLine("Enter first number");
 int number1 = int.Parse(ReadLine()!);
 
@@ -18,14 +21,30 @@ try
     int result = calculator.Calculate(number1, number2, operation);
     DisplayResult(result);
 }
+catch (ArgumentNullException ex) when (ex.ParamName == "operation")
+{
+    Console.WriteLine("Operation not provided. " + ex);
+
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine("An argument was not provided. " + ex);
+}
+catch (ArgumentOutOfRangeException ex)
+{
+    Console.WriteLine($"Operation not supported. {ex}");
+}
 catch (Exception ex)
 {
-    Console.WriteLine("Oops, something went wrong. " + ex.ToString());
+    Console.WriteLine("Oops, something went wrong. " + ex);
 }
 
 
 WriteLine("\nPress enter to exit.");
 ReadLine();
 
-
+static void HandleException(object sender, UnhandledExceptionEventArgs e)
+{
+    Console.WriteLine("Sorry, there was a problem. " + e.ExceptionObject);
+}
 static void DisplayResult(int result) => WriteLine($"Result is: {result}");
